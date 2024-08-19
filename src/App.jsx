471bfbfe59b3c9f518c467ede8f525b0a457a820
Ali-Oubraim <oubraima82@gmail.com>
2024-08-19 12:54:19 +0100

    [frontend] add socketSlice to the front

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
 import "./App.css";
 import ChatComponent from "./components/ChatComponent";
+import { setSocket } from "./features/socketSlice";
+import io from "socket.io-client";
+import { useDispatch } from "react-redux";
 
 function App() {
 
+  const dispatch = useDispatch(); 
+  useEffect(() => {
+    //  if (authUser) {
+    const socketio = io(`http://localhost:5000`, {
+      query: {
+        userId: "666c1c089a308c5fdb3169a6",
+      },
+    });
+    dispatch(setSocket(socketio));
+    //  socketio?.on("getOnlineUsers", (onlineUsers) => {
+    //    dispatch(setOnlineUsers(onlineUsers));
+    //  });
+    //  return () => socketio.close();
+    //  } else {
+    //    if (socket) {
+    //      socket.close();
+    //      dispatch(setSocket(null));
+    //    }
+    //  }
+  }, []);
   return (
     <>
       <ChatComponent />
